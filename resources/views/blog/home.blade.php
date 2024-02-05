e7cf6684221d85285a986dde1daf1fdbf1aec83c
Gabriel Hildebrandt Pereira <ghp010203@gmail.com>
2024-02-05 19:30:41 -0300

    primeiro commit

---
+++ resources/views/blog/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>BLOG</title>
+</head>
+<body class="--bs-BrancoFacilitaAi-color">
+    <h1>welcome</h1>
+    <button class="btn btn-purple">NÃO CLICK</button>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/app.js"></script>
+</body>
+</html>
